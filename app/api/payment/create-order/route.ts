1afefde5cf3a59ce51088cee6ac8eea577e14f1c
Anurag Mishra CS1 18 <anuragmishra239914@gmail.com>
2026-04-22 19:17:44 +0530

    change in create order

--- app/api/payment/create-order/route.ts
+++ app/api/payment/create-order/route.ts
@@ -1,15 +1,15 @@
 import Razorpay from "razorpay";
 import { NextResponse } from "next/server";
 
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID!,
+  key_secret: process.env.RAZORPAY_KEY_SECRET!,
+});
+
 export async function POST() {
   try {
-    const razorpay = new Razorpay({
-      key_id: process.env.RAZORPAY_KEY_ID!,
-      key_secret: process.env.RAZORPAY_KEY_SECRET!,
-    });
-
     const options = {
-      amount: 100, // ₹1
+      amount: 100, // ₹1 (in paise)
       currency: "INR",
       receipt: "order_rcptid_1",
     };
@@ -17,7 +17,6 @@ export async function POST() {
     const order = await razorpay.orders.create(options);
 
     return NextResponse.json({ order });
-
   } catch (error) {
     return NextResponse.json(
       { error: "Failed to create order" },
