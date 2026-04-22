9df19ffc7ca4b83209bcc420417c3a84c2e737ce
Anurag Mishra CS1 18 <anuragmishra239914@gmail.com>
2026-04-22 22:16:16 +0530

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,20 @@
 
 import { signOut } from "next-auth/react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
+  const router=useRouter();
   return (
     <div className="flex justify-between p-4 border-b">
       <h1 className="font-bold">Code Reviewer</h1>
 
       <div className="space-x-4">
-        <button onClick={()=>localStorage.removeItem("token")}>Logout</button>
+        <button onClick={()=>{
+          signOut();
+          localStorage.removeItem("token");
+          router.push("/auth/login");
+        }}>Logout</button>
         <Link href="/dashboard">Dashboard</Link>
         <Link href="/dashboard/history">History</Link>
       </div>
